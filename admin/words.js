ed269d76ce7e64bd5f5f6ae67b942a8e208399b8
Benjamin Beck <accounts@benjaminbeck.eu>
2024-05-26 16:23:37 +0200

    First

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'iouiapp adapter settings': {
+        'en': 'Adapter settings for iouiapp',
+        'de': 'Adaptereinstellungen für iouiapp',
+        'ru': 'Настройки адаптера для iouiapp',
+        'pt': 'Configurações do adaptador para iouiapp',
+        'nl': 'Adapterinstellingen voor iouiapp',
+        'fr': "Paramètres d'adaptateur pour iouiapp",
+        'it': "Impostazioni dell'adattatore per iouiapp",
+        'es': 'Ajustes del adaptador para iouiapp',
+        'pl': 'Ustawienia adaptera dla iouiapp',
+        'zh-cn': 'iouiapp的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
